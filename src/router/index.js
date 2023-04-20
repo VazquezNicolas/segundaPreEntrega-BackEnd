c56d98cd3164c5ba4b3f0cc644bf7896e043a4ca
VazquezNicolas <nicolasorlandovazquez@gmail.com>
2023-04-20 20:33:31 -0300

    corrijo rutas

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ const productsController = require ('./products/products.controller')
 const cartsController = require ('./carts/carts.controller')
 
 const router = app => {
-    app.use('/carts', cartsController)
-    app.use('/products', productsController)
+    app.use('/api/carts', cartsController)
+    app.use('/api/products', productsController)
 }
 
 module.exports = router
