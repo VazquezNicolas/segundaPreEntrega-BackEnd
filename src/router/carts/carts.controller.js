28d83be0d0c8279aced0e75022eade668a8b9b74
VazquezNicolas <nicolasorlandovazquez@gmail.com>
2023-04-20 18:22:30 -0300

    Elimino todos los productos del carrito

--- src/router/carts/carts.controller.js
+++ src/router/carts/carts.controller.js
@@ -6,6 +6,7 @@ const Products = require('../../model/Products.model');
 const router = Router()
 const cartFile = new FilesDao('products.json')
 
+//Mostrar todos los carritos
 router.get('/', async (req,res) => {
     try {
         const cart = await Carts.find()
@@ -16,22 +17,7 @@ router.get('/', async (req,res) => {
     }
 })
 
-// router.put('/:cid', async (req,res) => {
-//     try {
-//     const {cid} = req.params
-//     const {product} = req.body
-
-//     const cart = await Carts.findOne ({ _id: cid})
-//     cart.products.push({product})
-        
-//     const response = await Carts.updateOne({_id: cid}, cart)
-
-//     res.json({ message: response})
-//     } catch (error) {
-    
-//         res.status(400).json({status: 'error', error})
-//     }
-// })
+//Agregar producto a carrito
 router.put('/:cid/products/:pid', async (req,res) => {
     try {
     const {cid, pid} = req.params
@@ -49,7 +35,7 @@ router.put('/:cid/products/:pid', async (req,res) => {
     }
 })
 
-//Agregar carrito
+//Agregar carrito nuevo
 router.post('/', async (req,res) => {
     try {
         const sinEstoNoFunciona = null
@@ -64,6 +50,42 @@ router.post('/', async (req,res) => {
     }
 })
 
-//
+//Eliminar producto de un carrito
+router.delete('/:cid/products/:pid', async (req,res) => {
+    try {
+    const {cid, pid} = req.params
+
+    const cart = await Carts.findOne ({ _id: cid})
+    const product = await Products.findOne ({ _id: pid})
+    
+    console.log(product)
+    cart.products = await cart.products.filter(e => e !== product)
+        
+    await Carts.updateOne({_id: cid}, cart)
+
+    res.json({ message: "Producto eliminado"})
+    } catch (error) {
+    
+        console.log(error)
+        res.status(400).json({status: 'error', error})
+    }
+})
+
+//Eliminar todos los productos del carrito
+router.delete('/:cid', async (req,res) => {
+    try {
+    const {cid} = req.params
+
+    const cart = await Carts.findOne ({ _id: cid})
+    cart.products = []
+
+    await Carts.updateOne({_id: cid}, cart)
+
+    res.json({ message: "Se a vaciado el carrito!"})
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({status: 'error', error})
+    }
+})
 
 module.exports = router
