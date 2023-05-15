046ed26a89b8057d903bd6c624b68dcc75f8b398
VazquezNicolas <nicolasorlandovazquez@gmail.com>
2023-05-14 23:23:11 -0300

    agrego views

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require ('express');
 const mongoConnect = require('../db');
 const router = require('./router');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+const Handlebars = require("handlebars")
 
 const app = express()
 const port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname +'/public'))
 
-app.engine('handlebars', handlebars.engine())
+app.engine('handlebars', handlebars.engine({
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    })
+)
 app.set('views', __dirname+'/views')
 app.set('view engine', 'handlebars')
 
